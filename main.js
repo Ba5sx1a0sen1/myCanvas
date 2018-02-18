5be690c5389beaa48490bb27b67a2ce33ed448ed
BassXiaoSen <314121378@qq.com>
2018-02-19 03:38:33 +0800

    禁用滚动

--- main.js
+++ main.js
@@ -7,16 +7,48 @@ listenToUser(yyy)//设置绘画事件
 
 //橡皮擦与画笔切换操作
 var eraserEnabled = false
-eraser.onclick = function () {
-    eraserEnabled = true
-    actions.className = 'actions x'
+// eraser.onclick = function () {
+//     eraserEnabled = true
+//     actions.className = 'actions x'
 
-}
-brush.onclick = function () {
-    eraserEnabled = false
-    actions.className = 'actions'
+// }
+// brush.onclick = function () {
+//     eraserEnabled = false
+//     actions.className = 'actions'
+// }
+pen.onclick = function(){
+    eraserEnabled = false;
+    pen.classList.add('active')
+    eraser.classList.remove('active')
 }
 
+eraser.onclick = function(){
+    eraserEnabled = true;
+    eraser.classList.add('active')
+    pen.classList.remove('active')
+    
+}
+
+red.onclick = function(){
+    context.strokeStyle = 'red'
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')    
+}
+
+green.onclick = function(){
+    context.strokeStyle = 'green'
+    red.classList.remove('active')
+    green.classList.add('active')
+    blue.classList.remove('active')  
+}
+
+blue.onclick = function(){
+    context.strokeStyle = 'blue'
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.add('active')  
+}
 
 /******/
 //工具函数
@@ -38,14 +70,14 @@ function autoSetCanvasSize(canvas) {
 
 function drawCircle(x, y, radius) {
     context.beginPath()
-    context.fillStyle = 'black'
+    // context.fillStyle = 'black'
     context.arc(x, y, radius, 0, Math.PI * 2);
     context.fill()
 }
 
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
-    context.strokeStyle = 'black'
+    // context.strokeStyle = 'black'
     context.moveTo(x1, y1) // 起点
     context.lineWidth = 5 // 线的粗细大小
     context.lineTo(x2, y2) // 终点
